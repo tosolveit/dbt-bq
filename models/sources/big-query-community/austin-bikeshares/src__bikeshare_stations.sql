80883a26cd2e58b072350835d9fd96ac52163239
ikarapanca <ilkerkarapanca@gmail.com>
2023-06-13 20:28:29 +0200

    Feature/add austin bikerental public data (#2)
    * add bikeshare sources
    
    * add stg models

---
+++ models/sources/big-query-community/austin-bikeshares/src__bikeshare_stations.sql
@@ -0,0 +1,9 @@
+WITH source AS (
+  SELECT
+    *
+  FROM `bigquery-public-data.austin_bikeshare.bikeshare_stations`
+)
+
+SELECT
+    *
+FROM source
---
+++ models/sources/big-query-community/austin-bikeshares/src__bikeshare_trips.sql
@@ -0,0 +1,10 @@
+WITH source AS (
+  SELECT
+    *
+  FROM `bigquery-public-data.austin_bikeshare.bikeshare_trips`
+)
+
+SELECT
+    *
+FROM source
+
